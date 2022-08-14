2da6f1e7ac1c655bac54ab30c798b6bc0af1a688
Jorge Rodriguez <jr4436531@gmail.com>
2022-08-14 13:16:07 -0500

    commit v2 funcion albafetica

--- Laboratorio_cadenas_string.cpp
+++ Laboratorio_cadenas_string.cpp
@@ -17,7 +17,20 @@ cout<<"El numero de caracteres: "<<ncaracteres<<" no es divisible para: "<<numer
 
 
 void alfabeticosstring(string &cad){
-
+for (int i = 0; i < cad.length(); i++){
+     if (i==0){
+     cad[0]=toupper(cad[0]);
+     cout<<cad[0];
+     }
+    if (isblank(cad[i])){
+    cout<<" ";
+    cad[i+1]=toupper(cad[i+1]);
+    cout<<cad[i+1];
+    }
+    else{
+    cout<<cad[i+1];
+    }
+}
 
 }
 
@@ -28,7 +41,11 @@ void alfanumerico(string &cade){
 }
 
 main(){
+string  caden;
+int a=0, b=0, c=0;
+cout<<"Ingrese una cadena: ";
+getline (cin,caden);
+cout<<"La cadena ingresada es: "<<caden<<"\n";
 
 
-    
 }
